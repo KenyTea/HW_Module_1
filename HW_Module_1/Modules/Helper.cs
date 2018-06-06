077777013420cbb8704a6298a50cad8f3a319c2f
Alex <vassilyevalexey@gmail.com>
2018-06-06 09:45:46 +0600

    3

---
+++ HW_Module_1/Modules/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Module_1.Modules
+{
+   public class Helper
+    {
+        List<Item> listItem = new List<Item>();
+
+
+    }
+}
