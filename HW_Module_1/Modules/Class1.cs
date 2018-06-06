8a6733e0fd5b484f48ffaea84308faaae9731237
Alex <vassilyevalexey@gmail.com>
2018-06-06 09:03:28 +0600

    Add project files.

---
+++ HW_Module_1/Modules/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Module_1.Modules
+{
+    class Class1
+    {
+    }
+}
